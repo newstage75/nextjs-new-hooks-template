bb8f7c4c967b4417a9294017c707d413fae6fa8a
nagasawa hirotomo <hirotomo.nagasawa@gmail.com>
2024-12-01 15:50:34 +0900

    idをString型へ変更

---
+++ prisma/migrations/20241201064944_init/migration.sql
@@ -0,0 +1,97 @@
+/*
+  Warnings:
+
+  - The primary key for the `Follows` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Like` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Post` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Reply` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `bio` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `image` on the `User` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Follows" DROP CONSTRAINT "Follows_followerId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Follows" DROP CONSTRAINT "Follows_followingId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Like" DROP CONSTRAINT "Like_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Like" DROP CONSTRAINT "Like_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Reply" DROP CONSTRAINT "Reply_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Reply" DROP CONSTRAINT "Reply_postId_fkey";
+
+-- AlterTable
+ALTER TABLE "Follows" DROP CONSTRAINT "Follows_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "followerId" SET DATA TYPE TEXT,
+ALTER COLUMN "followingId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Follows_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Follows_id_seq";
+
+-- AlterTable
+ALTER TABLE "Like" DROP CONSTRAINT "Like_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ALTER COLUMN "postId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Like_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Like_id_seq";
+
+-- AlterTable
+ALTER TABLE "Post" DROP CONSTRAINT "Post_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "authorId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Post_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Post_id_seq";
+
+-- AlterTable
+ALTER TABLE "Reply" DROP CONSTRAINT "Reply_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "postId" SET DATA TYPE TEXT,
+ALTER COLUMN "authorId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Reply_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Reply_id_seq";
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "bio",
+DROP COLUMN "image",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Reply" ADD CONSTRAINT "Reply_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Reply" ADD CONSTRAINT "Reply_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Follows" ADD CONSTRAINT "Follows_followerId_fkey" FOREIGN KEY ("followerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Follows" ADD CONSTRAINT "Follows_followingId_fkey" FOREIGN KEY ("followingId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
